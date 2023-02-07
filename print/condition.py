3626fbeb6b85587d9ff92592d0690d2e6ceae9c8
zhenwei <zhenwei1108@163.com>
2023-02-07 23:27:04 +0800

    add for each

--- print/condition.py
+++ print/condition.py
@@ -1,7 +1,12 @@
 inputInt = int(input("please input a number"))
 if inputInt > 5 :
-    print("num is :"+inputInt)
+    print("num is :"+str(inputInt))
 elif inputInt <4:
-    print("please reput")
+    print("please reinput")
 elif inputInt == 4|inputInt==5:
     print("right")
+
+
+worlds = ['001', '002', '007', '008']
+for i in worlds:
+    print(i)
