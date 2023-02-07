b4a875d1ff9c18c5fc42569cbdef69cee5d6d61e
zhenwei <zhenwei1108@163.com>
2023-02-07 23:33:10 +0800

    完善条件判断

--- print/condition.py
+++ print/condition.py
@@ -10,3 +10,13 @@ elif inputInt == 4|inputInt==5:
 worlds = ['001', '002', '007', '008']
 for i in worlds:
     print(i)
+
+
+# 从0 - 5 遍历
+for i in range(5):
+    print(i)
+    if i >3:
+        break #跳出当前循环
+        print("over")
+    print("ok")
+print("bye bye")
