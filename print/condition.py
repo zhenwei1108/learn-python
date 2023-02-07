327c2e8fe542137257634dd429f105b0e417c0da
zhenwei <zhenwei1108@163.com>
2023-02-07 23:12:55 +0800

    条件语句

---
+++ print/condition.py
@@ -0,0 +1,7 @@
+inputInt = int(input("please input a number"))
+if inputInt > 5 :
+    print("num is :"+inputInt)
+elif inputInt <4:
+    print("please reput")
+elif inputInt == 4|inputInt==5:
+    print("right")
